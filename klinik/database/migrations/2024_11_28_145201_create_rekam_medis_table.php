6f668a77250620636a7f689bcb9d7f62a1883c97
totti-love <totti.prtama@gmail.com>
2024-11-28 22:31:40 +0700

    Migration table

---
+++ klinik/database/migrations/2024_11_28_145201_create_rekam_medis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rekam_medis', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('kode', 10);
+            $table->date('tanggal');
+            $table->uuid('pasien_id');
+            $table->foreign('pasien_id')->references('id')->on('pasiens');
+            $table->uuid('dokter_id');
+            $table->foreign('dokter_id')->references('id')->on('dokters');
+            $table->uuid('obat_id');
+            $table->foreign('obat_id')->references('id')->on('obats');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rekam_medis');
+    }
+};
